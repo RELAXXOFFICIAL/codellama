3dcfb5e32d2a3b9159066b9994121b125df3c241
RELAXXOFFICIAL <128732812+RELAXXOFFICIAL@users.noreply.github.com>
2023-10-18 04:50:50 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-read -p "Enter the URL from email: " PRESIGNED_URL
+read -p "Enter the URL from email: 'https://download2.llamameta.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoicHJ0OWw2aDRyZHJ2YWpwM3EyMm91b3g0IiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZG93bmxvYWQyLmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2OTcwOTE0NDJ9fX1dfQ__&Signature=QZkgvoc9d%7EU0z5qL1mzo-1-sUe-bu6FZpYJ7hs1z-zSFuIdFzdU5xfIKdZgVr7h7SR7tTCeiwKxUW62-OnyGFQDeYVX8H6SFgyxYW0rUQxajoQI3qMuSK3-%7ENsBmiWmwOb37BFw9oxkvO1FbSKlmBQeQF3VbqgUneHr0sQuGqDTF98EuVY8-r0Ey5qWPeSb3eobt7epyNB69e5ZNBqAlxHkjSbRkmAM%7EIndYeTdcBvp1Y1we%7EtYR2UdnSjyEexcwoFVKnNG182U4AleH3za133pZW9r6QhzOEnQyI-qqwbgb7KhAEDERFKPonyGWkspu59kgdT49AZP-fvM7GDdDfg__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=241886448506760' " PRESIGNED_URL
 echo ""
 ALL_MODELS="7b,13b,34b,7b-Python,13b-Python,34b-Python,7b-Instruct,13b-Instruct,34b-Instruct"
 read -p "Enter the list of models to download without spaces ($ALL_MODELS), or press Enter for all: " MODEL_SIZE
